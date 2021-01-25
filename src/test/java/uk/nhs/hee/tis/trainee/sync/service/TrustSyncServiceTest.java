ffd6f96649c00b1602152ef26788a1336919cd11
Andy Dingley <Judge40@users.noreply.github.com>
2021-01-25 14:13:55 +0000

    feat: Store Trusts when received
    TIS21-1032

---
+++ src/test/java/uk/nhs/hee/tis/trainee/sync/service/TrustSyncServiceTest.java
@@ -0,0 +1,80 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2021 Crown Copyright (Health Education England)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.nhs.hee.tis.trainee.sync.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import uk.nhs.hee.tis.trainee.sync.model.Record;
+import uk.nhs.hee.tis.trainee.sync.model.Trust;
+import uk.nhs.hee.tis.trainee.sync.repository.TrustRepository;
+
+class TrustSyncServiceTest {
+
+  private TrustSyncService service;
+
+  private TrustRepository repository;
+
+  @BeforeEach
+  void setUp() {
+    repository = mock(TrustRepository.class);
+    service = new TrustSyncService(repository);
+  }
+
+  @Test
+  void shouldThrowExceptionIfRecordNotPost() {
+    Record record = new Record();
+    assertThrows(IllegalArgumentException.class, () -> service.syncRecord(record));
+  }
+
+  @ParameterizedTest(
+      name = "Should store trusts when operation is {0}.")
+  @ValueSource(strings = {"load", "insert", "update"})
+  void shouldStoreTrusts(String operation) {
+    Trust record = new Trust();
+    record.setTisId("idValue");
+    record.setOperation(operation);
+
+    service.syncRecord(record);
+
+    verify(repository).save(record);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldDeleteTrustFromStore() {
+    Trust record = new Trust();
+    record.setTisId("idValue");
+    record.setOperation("delete");
+
+    service.syncRecord(record);
+
+    verify(repository).deleteById("idValue");
+    verifyNoMoreInteractions(repository);
+  }
+}
