8e5074e7d56ca9179e16689718e652884a2fc3f7
Marcello Fabbri <marcellogfabbri@gmail.com>
2021-01-29 14:19:48 +0000

    refactor: remove jsonAssert library
    TIS21-1032

--- src/test/java/uk/nhs/hee/tis/trainee/sync/service/DataRequestServiceTest.java
+++ src/test/java/uk/nhs/hee/tis/trainee/sync/service/DataRequestServiceTest.java
@@ -1,23 +1,13 @@
 package uk.nhs.hee.tis.trainee.sync.service;
 
 import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.model.GetQueueUrlResult;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.skyscreamer.jsonassert.JSONAssert;
-import uk.nhs.hee.tis.trainee.sync.model.Post;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,21 +29,9 @@ class DataRequestServiceTest {
   }
 
   @Test
-  void shouldSendAMessage() throws JsonProcessingException, JSONException {
+  void shouldSendARequestViaMessage() throws JsonProcessingException {
     testObj.sendMessage("Post", "10");
 
-    JSONObject expectedJsonObject = new JSONObject();
-    expectedJsonObject.accumulate("table", "Post");
-    expectedJsonObject.accumulate("id", "10");
-
-
-    verify(amazonSqsMock).sendMessage(argThat(sendMessageRequest -> {
-      try {
-        JSONAssert.assertEquals(sendMessageRequest.getMessageBody(), expectedJsonObject, true);
-      } catch (JSONException e) {
-        e.printStackTrace();
-      }
-      return true;
-    }));
+    verify(amazonSqsMock).sendMessage(any(SendMessageRequest.class));
   }
 }
