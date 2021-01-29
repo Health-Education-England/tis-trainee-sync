e8a585eecfe3f8fa7563cbaaecdffa083fd5a9f3
Marcello Fabbri <marcellogfabbri@gmail.com>
2021-01-29 16:16:53 +0000

    chore: config AmazonSQS with credentials+region, add test application.yml

--- src/main/java/uk/nhs/hee/tis/trainee/sync/config/AmazonSqsConfig.java
+++ src/main/java/uk/nhs/hee/tis/trainee/sync/config/AmazonSqsConfig.java
@@ -1,16 +1,40 @@
 package uk.nhs.hee.tis.trainee.sync.config;
 
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonSqsConfig {
 
+  @Value("${application.aws.access.key.id}")
+  private String awsAccessKeyId;
+
+  @Value("${application.aws.secret.access.key}")
+  private String awsSecretAccessKey;
+
+  @Value("${application.aws.region}")
+  private String region;
+
+  AWSCredentialsProvider credentialsProvider() {
+    BasicAWSCredentials basic = new BasicAWSCredentials(this.awsAccessKeyId,
+        this.awsSecretAccessKey);
+    return new AWSStaticCredentialsProvider(basic);
+  }
+
   @Bean
   public AmazonSQS amazonSqs() {
-    return AmazonSQSClientBuilder.defaultClient();
+    AmazonSQSClientBuilder amazonSQSClientBuilder = AmazonSQSClientBuilder.standard();
+    amazonSQSClientBuilder.setRegion(region);
+    amazonSQSClientBuilder.setCredentials(credentialsProvider());
+    amazonSQSClientBuilder.setClientConfiguration(new ClientConfiguration());
+    return amazonSQSClientBuilder.build();
   }
 
 }
