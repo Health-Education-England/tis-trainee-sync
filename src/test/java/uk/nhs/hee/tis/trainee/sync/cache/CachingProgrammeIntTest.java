cddc078ae3caf18b16a54b1d654bbf96837a8341
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2021-02-24 09:55:26 +0000

    Added basic tests
    Note: the cache integration tests are very repetitive and should be
    refactored. This may also hold true for some of the other event listener
    and sync service tests.

---
+++ src/test/java/uk/nhs/hee/tis/trainee/sync/cache/CachingProgrammeIntTest.java
@@ -0,0 +1,157 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2021 Crown Copyright (Health Education England)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.nhs.hee.tis.trainee.sync.cache;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.annotation.DirtiesContext;
+import uk.nhs.hee.tis.trainee.sync.config.MongoConfiguration;
+import uk.nhs.hee.tis.trainee.sync.model.Operation;
+import uk.nhs.hee.tis.trainee.sync.model.Programme;
+import uk.nhs.hee.tis.trainee.sync.repository.ProgrammeRepository;
+import uk.nhs.hee.tis.trainee.sync.service.ProgrammeSyncService;
+
+
+@SpringBootTest
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
+class CachingProgrammeIntTest {
+
+  private static final String PROGRAMME_FORDY = "fordy";
+
+  // We require access to the mock before the proxy wraps it.
+  private static ProgrammeRepository mockProgrammeRepository;
+
+  @Autowired
+  ProgrammeSyncService programmeSyncService;
+
+  @Autowired
+  CacheManager cacheManager;
+
+  private Cache programmeCache;
+
+  private Programme programme;
+
+  @BeforeEach
+  void setup() {
+    programme = new Programme();
+    programme.setTisId(PROGRAMME_FORDY);
+    programme.setOperation(Operation.DELETE);
+    programme.setTable(Programme.ENTITY_NAME);
+
+    programmeCache = cacheManager.getCache(Programme.ENTITY_NAME);
+  }
+
+  @Test
+  void shouldHitCacheOnSecondInvocation() {
+    when(mockProgrammeRepository.findById(PROGRAMME_FORDY))
+        .thenReturn(Optional.of(programme), Optional.of(new Programme()));
+    assertThat(programmeCache.get(PROGRAMME_FORDY)).isNull();
+
+    Optional<Programme> actual1 = programmeSyncService.findById(PROGRAMME_FORDY);
+    assertThat(programmeCache.get(PROGRAMME_FORDY)).isNotNull();
+    Optional<Programme> actual2 = programmeSyncService.findById(PROGRAMME_FORDY);
+
+    verify(mockProgrammeRepository).findById(PROGRAMME_FORDY);
+    assertThat(actual1).isPresent().get().isEqualTo(programme).isEqualTo(actual2.orElseThrow());
+  }
+
+  @Test
+  void shouldEvictWhenDeletedAndMissCacheOnNextInvocation() {
+    Programme otherProgramme = new Programme();
+    final String otherKey = "Foo";
+    otherProgramme.setTisId(otherKey);
+    otherProgramme.setOperation(Operation.LOAD);
+    when(mockProgrammeRepository.findById(otherKey)).thenReturn(Optional.of(otherProgramme));
+    programmeSyncService.findById(otherKey);
+    assertThat(programmeCache.get(otherKey)).isNotNull();
+    when(mockProgrammeRepository.findById(PROGRAMME_FORDY)).thenReturn(Optional.of(programme));
+    programmeSyncService.findById(PROGRAMME_FORDY);
+    assertThat(programmeCache.get(PROGRAMME_FORDY)).isNotNull();
+
+    programmeSyncService.syncRecord(programme);
+    assertThat(programmeCache.get(PROGRAMME_FORDY)).isNull();
+    assertThat(programmeCache.get(otherKey)).isNotNull();
+    verify(mockProgrammeRepository).deleteById(PROGRAMME_FORDY);
+
+    programmeSyncService.findById(PROGRAMME_FORDY);
+    assertThat(programmeCache.get(PROGRAMME_FORDY)).isNotNull();
+
+    verify(mockProgrammeRepository, times(2)).findById(PROGRAMME_FORDY);
+  }
+
+  @Test
+  void shouldReplaceCacheWhenSaved() {
+    Programme staleProgramme = new Programme();
+    staleProgramme.setTisId(PROGRAMME_FORDY);
+    staleProgramme.setTable("Stale");
+    staleProgramme.setOperation(Operation.UPDATE);
+    when(mockProgrammeRepository.save(staleProgramme)).thenReturn(staleProgramme);
+    programmeSyncService.syncRecord(staleProgramme);
+    assertThat(programmeCache.get(PROGRAMME_FORDY).get()).isEqualTo(staleProgramme);
+
+    Programme updateProgramme = new Programme();
+    updateProgramme.setTable(Programme.ENTITY_NAME);
+    updateProgramme.setTisId(PROGRAMME_FORDY);
+    updateProgramme.setOperation(Operation.UPDATE);
+    when(mockProgrammeRepository.save(updateProgramme)).thenReturn(programme);
+
+    programmeSyncService.syncRecord(updateProgramme);
+    assertThat(programmeCache.get(PROGRAMME_FORDY).get()).isEqualTo(programme);
+    verify(mockProgrammeRepository).save(staleProgramme);
+    verify(mockProgrammeRepository).save(updateProgramme);
+  }
+
+  @TestConfiguration
+  static class Configuration {
+
+    @Primary
+    @Bean
+    ProgrammeRepository mockProgrammeRepository() {
+      mockProgrammeRepository = mock(ProgrammeRepository.class);
+      return mockProgrammeRepository;
+    }
+
+    ////// Mocks to enable application context //////
+    @MockBean
+    AmazonSQS amazonSqs;
+    @MockBean
+    private MongoConfiguration mongoConfiguration;
+    /////////////////////////////////////////////////
+  }
+}
---
+++ src/test/java/uk/nhs/hee/tis/trainee/sync/service/ProgrammeMembershipSyncServiceTest.java
@@ -0,0 +1,276 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2021 Crown Copyright (Health Education England)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.nhs.hee.tis.trainee.sync.service;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.atMostOnce;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static uk.nhs.hee.tis.trainee.sync.model.Operation.DELETE;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.Collections;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import uk.nhs.hee.tis.trainee.sync.model.Operation;
+import uk.nhs.hee.tis.trainee.sync.model.ProgrammeMembership;
+import uk.nhs.hee.tis.trainee.sync.model.Record;
+import uk.nhs.hee.tis.trainee.sync.repository.ProgrammeMembershipRepository;
+
+class ProgrammeMembershipSyncServiceTest {
+
+  private static final String ID = "40";
+
+  private static final String personId = "1";
+  private static final String programmeId = "1";
+  private static final String programmeMembershipType = "SUBSTANTIVE";
+  private static final String programmeStartDate = "2020-01-01";
+  private static final String programmeEndDate = "2021-01-02";
+
+  private ProgrammeMembershipSyncService service;
+
+  private ProgrammeMembershipRepository repository;
+
+  private ProgrammeMembership record;
+
+  private DataRequestService dataRequestService;
+
+  private PostSyncService postSyncService;
+
+  @BeforeEach
+  void setUp() {
+    dataRequestService = mock(DataRequestService.class);
+    postSyncService = mock(PostSyncService.class);
+    repository = mock(ProgrammeMembershipRepository.class);
+    service = new ProgrammeMembershipSyncService(repository, dataRequestService);
+
+    record = new ProgrammeMembership();
+    record.setTisId(ID);
+  }
+
+  @Test
+  void shouldThrowExceptionIfRecordNotProgrammeMembership() {
+    Record record = new Record();
+    assertThrows(IllegalArgumentException.class, () -> service.syncRecord(record));
+  }
+
+  @ParameterizedTest(name = "Should store records when operation is {0}.")
+  @EnumSource(value = Operation.class, names = {"LOAD", "INSERT", "UPDATE"})
+  void shouldStoreRecords(Operation operation) {
+    record.setOperation(operation);
+
+    service.syncRecord(record);
+
+    verify(repository).save(record);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldDeleteRecordFromStore() {
+    record.setOperation(DELETE);
+
+    service.syncRecord(record);
+
+    verify(repository).deleteById(ID);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldFindRecordByPersonIdWhenExists() {
+    when(repository.findByPersonId(ID)).thenReturn(Collections.singleton(record));
+
+    Set<ProgrammeMembership> foundRecords = service.findByPersonId(ID);
+    assertThat("Unexpected record count.", foundRecords.size(), is(1));
+
+    ProgrammeMembership foundRecord = foundRecords.iterator().next();
+    assertThat("Unexpected record.", foundRecord, sameInstance(record));
+
+    verify(repository).findByPersonId(ID);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldNotFindRecordByIdPersonWhenNotExists() {
+    when(repository.findByPersonId(ID)).thenReturn(Collections.emptySet());
+
+    Set<ProgrammeMembership> foundRecords = service.findByPersonId(ID);
+    assertThat("Unexpected record count.", foundRecords.size(), is(0));
+
+    verify(repository).findByPersonId(ID);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldFindRecordByCurriculumIdWhenExists() {
+    when(repository.findByCurriculumId(ID)).thenReturn(Collections.singleton(record));
+
+    Set<ProgrammeMembership> foundRecords = service.findByCurriculumId(ID);
+    assertThat("Unexpected record count.", foundRecords.size(), is(1));
+
+    ProgrammeMembership foundRecord = foundRecords.iterator().next();
+    assertThat("Unexpected record.", foundRecord, sameInstance(record));
+
+    verify(repository).findByCurriculumId(ID);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldNotFindRecordByIdCurriculumWhenNotExists() {
+    when(repository.findByCurriculumId(ID)).thenReturn(Collections.emptySet());
+
+    Set<ProgrammeMembership> foundRecords = service.findByCurriculumId(ID);
+    assertThat("Unexpected record count.", foundRecords.size(), is(0));
+
+    verify(repository).findByCurriculumId(ID);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldFindRecordByProgrammeIdWhenExists() {
+    when(repository.findByProgrammeId(ID)).thenReturn(Collections.singleton(record));
+
+    Set<ProgrammeMembership> foundRecords = service.findByProgrammeId(ID);
+    assertThat("Unexpected record count.", foundRecords.size(), is(1));
+
+    ProgrammeMembership foundRecord = foundRecords.iterator().next();
+    assertThat("Unexpected record.", foundRecord, sameInstance(record));
+
+    verify(repository).findByProgrammeId(ID);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldNotFindRecordByIdProgrammeWhenNotExists() {
+    when(repository.findByProgrammeId(ID)).thenReturn(Collections.emptySet());
+
+    Set<ProgrammeMembership> foundRecords = service.findByProgrammeId(ID);
+    assertThat("Unexpected record count.", foundRecords.size(), is(0));
+
+    verify(repository).findByProgrammeId(ID);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldFindRecordByPersonIdAndProgrammeIdAndProgrammeMembershipTypeAndProgrammeStartDateAndProgrammeEndDateWhenExists() {
+
+    when(repository.findByPersonIdAndProgrammeIdAndProgrammeMembershipTypeAndProgrammeStartDateAndProgrammeEndDate(personId,
+    programmeId, programmeMembershipType, programmeStartDate, programmeEndDate)).thenReturn(Collections.singleton(record));
+
+    Set<ProgrammeMembership> foundRecords = service.findByPersonIdAndProgrammeIdAndProgrammeMembershipTypeAndProgrammeStartDateAndProgrammeEndDate(personId,
+        programmeId, programmeMembershipType, programmeStartDate, programmeEndDate);
+    assertThat("Unexpected record count.", foundRecords.size(), is(1));
+
+    ProgrammeMembership foundRecord = foundRecords.iterator().next();
+    assertThat("Unexpected record.", foundRecord, sameInstance(record));
+
+    verify(repository).findByPersonIdAndProgrammeIdAndProgrammeMembershipTypeAndProgrammeStartDateAndProgrammeEndDate(personId,
+        programmeId, programmeMembershipType, programmeStartDate, programmeEndDate);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldNotFindRecordByPersonIdAndProgrammeIdAndProgrammeMembershipTypeAndProgrammeStartDateAndProgrammeEndDateWhenNotExists() {
+    when(repository.findByPersonIdAndProgrammeIdAndProgrammeMembershipTypeAndProgrammeStartDateAndProgrammeEndDate(personId,
+        programmeId, programmeMembershipType, programmeStartDate, programmeEndDate)).thenReturn(Collections.emptySet());
+
+    Set<ProgrammeMembership> foundRecords = service.findByPersonIdAndProgrammeIdAndProgrammeMembershipTypeAndProgrammeStartDateAndProgrammeEndDate(personId,
+        programmeId, programmeMembershipType, programmeStartDate, programmeEndDate);
+    assertThat("Unexpected record count.", foundRecords.size(), is(0));
+
+    verify(repository).findByPersonIdAndProgrammeIdAndProgrammeMembershipTypeAndProgrammeStartDateAndProgrammeEndDate(personId,
+        programmeId, programmeMembershipType, programmeStartDate, programmeEndDate);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldSendRequestWhenNotAlreadyRequested() throws JsonProcessingException {
+    service.request(ID);
+    verify(dataRequestService).sendRequest("ProgrammeMembership", ID);
+  }
+
+  @Test
+  void shouldNotSendRequestWhenAlreadyRequested() throws JsonProcessingException {
+    service.request(ID);
+    service.request(ID);
+    verify(dataRequestService, atMostOnce()).sendRequest("ProgrammeMembership", ID);
+    verifyNoMoreInteractions(dataRequestService);
+  }
+
+  @Test
+  void shouldSendRequestWhenSyncedBetweenRequests() throws JsonProcessingException {
+    service.request(ID);
+
+    record.setOperation(DELETE);
+    service.syncRecord(record);
+
+    service.request(ID);
+    verify(dataRequestService, times(2)).sendRequest("ProgrammeMembership", ID);
+  }
+
+  @Test
+  void shouldSendRequestWhenRequestedDifferentIds() throws JsonProcessingException {
+    service.request(ID);
+    service.request("140");
+    verify(dataRequestService, atMostOnce()).sendRequest("ProgrammeMembership", ID);
+    verify(dataRequestService, atMostOnce()).sendRequest("ProgrammeMembership", "140");
+  }
+
+  @Test
+  void shouldSendRequestWhenFirstRequestFails() throws JsonProcessingException {
+    doThrow(JsonProcessingException.class).when(dataRequestService)
+        .sendRequest(anyString(), anyString());
+
+    service.request(ID);
+    service.request(ID);
+
+    verify(dataRequestService, times(2)).sendRequest("ProgrammeMembership", ID);
+  }
+
+  @Test
+  void shouldCatchAJsonProcessingExceptionIfThrown() throws JsonProcessingException {
+    doThrow(JsonProcessingException.class).when(dataRequestService)
+        .sendRequest(anyString(), anyString());
+    assertDoesNotThrow(() -> service.request(ID));
+  }
+
+  @Test
+  void shouldThrowAnExceptionIfNotJsonProcessingException() throws JsonProcessingException {
+    IllegalStateException illegalStateException = new IllegalStateException("error");
+    doThrow(illegalStateException).when(dataRequestService).sendRequest(anyString(),
+        anyString());
+    assertThrows(IllegalStateException.class, () -> service.request(ID));
+    assertEquals("error", illegalStateException.getMessage());
+  }
+}
