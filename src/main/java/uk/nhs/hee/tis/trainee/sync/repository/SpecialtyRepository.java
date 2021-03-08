6ade07ae27d58e78963a408bf167df0da4801e85
Marcello Fabbri <marcellogfabbri@gmail.com>
2021-03-08 09:30:56 +0000

    test: SpecialtyEventListenerTest
    TIS21-1219

--- src/main/java/uk/nhs/hee/tis/trainee/sync/repository/SpecialtyRepository.java
+++ src/main/java/uk/nhs/hee/tis/trainee/sync/repository/SpecialtyRepository.java
@@ -1,12 +1,16 @@
 package uk.nhs.hee.tis.trainee.sync.repository;
 
 import java.util.Optional;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import uk.nhs.hee.tis.trainee.sync.model.Specialty;
 
+@CacheConfig(cacheNames = Specialty.ENTITY_NAME)
+@Repository
 public interface SpecialtyRepository extends MongoRepository<Specialty, String> {
 
   @Cacheable
