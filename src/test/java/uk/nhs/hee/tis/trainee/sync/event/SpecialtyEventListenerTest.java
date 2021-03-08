e98a2c7380e09d94963b831ff5c95628a259f09d
Marcello Fabbri <marcellogfabbri@gmail.com>
2021-03-08 09:30:56 +0000

    test: SpecialtyEventListenerTest
    TIS21-1219

---
+++ src/test/java/uk/nhs/hee/tis/trainee/sync/event/SpecialtyEventListenerTest.java
@@ -0,0 +1,34 @@
+package uk.nhs.hee.tis.trainee.sync.event;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
+import uk.nhs.hee.tis.trainee.sync.facade.PlacementEnricherFacade;
+import uk.nhs.hee.tis.trainee.sync.model.Specialty;
+
+public class SpecialtyEventListenerTest {
+
+  private SpecialtyEventListener listener;
+  private PlacementEnricherFacade enricher;
+
+  @BeforeEach
+  void setUp() {
+    enricher = mock(PlacementEnricherFacade.class);
+    listener = new SpecialtyEventListener(enricher);
+  }
+
+  @Test
+  void shouldCallEnricherAfterSave() {
+    Specialty record = new Specialty();
+    AfterSaveEvent<Specialty> event = new AfterSaveEvent<>(record, null, null);
+
+    listener.onAfterSave(event);
+
+    verify(enricher).enrich(record);
+    verifyNoMoreInteractions(enricher);
+  }
+}
