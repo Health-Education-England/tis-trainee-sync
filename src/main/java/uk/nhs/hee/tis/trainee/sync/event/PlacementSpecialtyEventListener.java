cadaf7765cae2ac5903d01100b3712b9c54fa1c1
Marcello Fabbri <marcellogfabbri@gmail.com>
2021-04-12 15:20:28 +0100

    feat: handle placementSpecialty deletions
    TIS21-1219

--- src/main/java/uk/nhs/hee/tis/trainee/sync/event/PlacementSpecialtyEventListener.java
+++ src/main/java/uk/nhs/hee/tis/trainee/sync/event/PlacementSpecialtyEventListener.java
@@ -1,12 +1,17 @@
 package uk.nhs.hee.tis.trainee.sync.event;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.AfterDeleteEvent;
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
+import org.springframework.data.mongodb.core.mapping.event.BeforeDeleteEvent;
 import org.springframework.stereotype.Component;
 import uk.nhs.hee.tis.trainee.sync.facade.PlacementEnricherFacade;
+import uk.nhs.hee.tis.trainee.sync.model.Placement;
 import uk.nhs.hee.tis.trainee.sync.model.PlacementSpecialty;
+import uk.nhs.hee.tis.trainee.sync.service.PlacementSyncService;
 
+@Slf4j
 @Component
 public class PlacementSpecialtyEventListener extends
     AbstractMongoEventListener<PlacementSpecialty> {
@@ -27,7 +32,16 @@ public class PlacementSpecialtyEventListener extends
 
   @Override
   public void onAfterDelete(AfterDeleteEvent<PlacementSpecialty> event) {
-    // TODO: Implement.
+    // TODO: make sure that:
+    //  1) the Placement was also deleted (OR)
+    //  2) the Placement has a new placementSpecialty
+
+    String placementId = (String) event.getDocument().get("_id");
+    boolean placementSpecialtyDeletedCorrectly = placementEnricher.placementSpecialtyDeletedCorrectly(placementId);
+    if (!placementSpecialtyDeletedCorrectly) {
+      log.warn("placementSpecialty with placementId {} deleted incorrectly", placementId);
+    }
+
     super.onAfterDelete(event);
   }
 }
