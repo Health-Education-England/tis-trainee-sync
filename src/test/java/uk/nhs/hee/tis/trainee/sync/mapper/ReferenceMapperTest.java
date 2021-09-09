f77d9cbd23a2ff75554f995fe802bdc217390da2
Andy Dingley <Judge40@users.noreply.github.com>
2021-09-09 17:18:14 +0100

    Revert "Merge pull request #151 from Health-Education-England/fix/booleanHandling"
    This reverts commit 6c330ba906b69af3128a8ff80773396edad7f2c9, reversing
    changes made to b1d96e505ff62b23e1ec651543a632a3a2053dfc.

--- src/test/java/uk/nhs/hee/tis/trainee/sync/mapper/ReferenceMapperTest.java
+++ src/test/java/uk/nhs/hee/tis/trainee/sync/mapper/ReferenceMapperTest.java
@@ -51,7 +51,7 @@ class ReferenceMapperTest {
   @Test
   void shouldMapInternalToBooleanWhenTrue() {
     Record recrd = new Record();
-    recrd.setData(Collections.singletonMap("internal", "1"));
+    recrd.setData(Collections.singletonMap("internal", "true"));
 
     ReferenceDto reference = mapper.toReference(recrd);
 
@@ -61,7 +61,7 @@ class ReferenceMapperTest {
   @Test
   void shouldMapInternalToBooleanWhenFalse() {
     Record recrd = new Record();
-    recrd.setData(Collections.singletonMap("internal", "0"));
+    recrd.setData(Collections.singletonMap("internal", "false"));
 
     ReferenceDto reference = mapper.toReference(recrd);
 
@@ -98,7 +98,7 @@ class ReferenceMapperTest {
   @Test
   void shouldMapPlacementGradeToBooleanWhenTrue() {
     Record recrd = new Record();
-    recrd.setData(Collections.singletonMap("placementGrade", "1"));
+    recrd.setData(Collections.singletonMap("placementGrade", "true"));
 
     ReferenceDto reference = mapper.toReference(recrd);
 
@@ -108,7 +108,7 @@ class ReferenceMapperTest {
   @Test
   void shouldMapPlacementGradeToBooleanWhenFalse() {
     Record recrd = new Record();
-    recrd.setData(Collections.singletonMap("placementGrade", "0"));
+    recrd.setData(Collections.singletonMap("placementGrade", "false"));
 
     ReferenceDto reference = mapper.toReference(recrd);
 
@@ -125,7 +125,7 @@ class ReferenceMapperTest {
   @Test
   void shouldMapTrainingGradeToBooleanWhenTrue() {
     Record recrd = new Record();
-    recrd.setData(Collections.singletonMap("trainingGrade", "1"));
+    recrd.setData(Collections.singletonMap("trainingGrade", "true"));
 
     ReferenceDto reference = mapper.toReference(recrd);
 
@@ -135,7 +135,7 @@ class ReferenceMapperTest {
   @Test
   void shouldMapTrainingGradeToBooleanWhenFalse() {
     Record recrd = new Record();
-    recrd.setData(Collections.singletonMap("trainingGrade", "0"));
+    recrd.setData(Collections.singletonMap("trainingGrade", "false"));
 
     ReferenceDto reference = mapper.toReference(recrd);
 
