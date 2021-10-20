a2af797e496cd24df6d46de93538f5ba0f9c0080
Marcello Fabbri <marcellogfabbri@gmail.com>
2021-10-20 09:50:10 +0100

    chore: Undo latest change to AmazonSqsAsync
    Make AmazonSqsAsync @Primary as before
    
    TIS21-2116

--- src/main/java/uk/nhs/hee/tis/trainee/sync/config/AmazonSqsConfig.java
+++ src/main/java/uk/nhs/hee/tis/trainee/sync/config/AmazonSqsConfig.java
@@ -42,6 +42,7 @@ public class AmazonSqsConfig {
    * @return The created bean.
    */
   @Bean
+  @Primary
   public AmazonSQSAsync amazonSqsAsync() {
     return AmazonSQSAsyncClientBuilder.defaultClient();
   }
