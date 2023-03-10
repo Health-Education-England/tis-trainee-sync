c14f530e6caa8ea7619a2140a421383521557262
EdwardBarclay <edward.barclay@hee.nhs.uk>
2023-03-10 10:30:26 +0000

    refactor: the last import re-arrangment I swear

--- src/main/java/uk/nhs/hee/tis/trainee/sync/service/CurriculumSyncService.java
+++ src/main/java/uk/nhs/hee/tis/trainee/sync/service/CurriculumSyncService.java
@@ -22,6 +22,7 @@
 package uk.nhs.hee.tis.trainee.sync.service;
 
 import static uk.nhs.hee.tis.trainee.sync.model.Operation.DELETE;
+
 import com.amazonaws.xray.spring.aop.XRayEnabled;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.Map;
