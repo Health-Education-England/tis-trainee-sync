5e0d6817d7b5faf4fa91d68cb790d1ba1e5164de
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2024-05-29 16:08:45 +0100

    code smell

--- src/test/java/uk/nhs/hee/tis/trainee/sync/service/DataRequestServiceTest.java
+++ src/test/java/uk/nhs/hee/tis/trainee/sync/service/DataRequestServiceTest.java
@@ -25,7 +25,6 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
