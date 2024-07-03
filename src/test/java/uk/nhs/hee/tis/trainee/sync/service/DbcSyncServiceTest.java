7742bc98dc2c947c736b2eef85725e05c1e69d75
Reuben Roberts <reuben.roberts@hee.nhs.uk>
2024-07-03 10:41:59 +0100

    feat: DBC sync service tests

---
+++ src/test/java/uk/nhs/hee/tis/trainee/sync/service/DbcSyncServiceTest.java
@@ -0,0 +1,215 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2024 Crown Copyright (Health Education England)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.nhs.hee.tis.trainee.sync.service;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.atMostOnce;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static uk.nhs.hee.tis.trainee.sync.model.Operation.DELETE;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.Map;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import uk.nhs.hee.tis.trainee.sync.model.Dbc;
+import uk.nhs.hee.tis.trainee.sync.model.Operation;
+import uk.nhs.hee.tis.trainee.sync.model.Record;
+import uk.nhs.hee.tis.trainee.sync.repository.DbcRepository;
+
+class DbcSyncServiceTest {
+
+  public static final String ID_2 = "140";
+  private static final String ID = "40";
+  private DbcSyncService service;
+
+  private DbcRepository repository;
+
+  private DataRequestService dataRequestService;
+
+  private ReferenceSyncService referenceSyncService;
+
+  private RequestCacheService requestCacheService;
+
+  private Dbc dbc;
+
+  private Map<String, String> whereMap;
+
+  private Map<String, String> whereMap2;
+
+  @BeforeEach
+  void setUp() {
+    repository = mock(DbcRepository.class);
+    dataRequestService = mock(DataRequestService.class);
+    referenceSyncService = mock(ReferenceSyncService.class);
+    requestCacheService = mock(RequestCacheService.class);
+
+    service = new DbcSyncService(repository, dataRequestService, referenceSyncService,
+        requestCacheService);
+
+    dbc = new Dbc();
+    dbc.setTisId(ID);
+
+    whereMap = Map.of("id", ID);
+    whereMap2 = Map.of("id", ID_2);
+  }
+
+  @Test
+  void shouldThrowExceptionIfRecordNotDbc() {
+    Record recrd = new Record();
+    assertThrows(IllegalArgumentException.class, () -> service.syncRecord(recrd));
+  }
+
+  @ParameterizedTest(name = "Should store records when operation is {0}.")
+  @EnumSource(value = Operation.class, names = {"LOAD", "INSERT", "UPDATE"})
+  void shouldStoreRecords(Operation operation) {
+    dbc.setOperation(operation);
+
+    service.syncRecord(dbc);
+
+    verify(repository).save(dbc);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldDeleteRecordFromStore() {
+    dbc.setOperation(DELETE);
+
+    service.syncRecord(dbc);
+
+    verify(repository).deleteById(ID);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldFindRecordByIdWhenExists() {
+    when(repository.findById(ID)).thenReturn(Optional.of(dbc));
+
+    Optional<Dbc> found = service.findById(ID);
+    assertThat("Record not found.", found.isPresent(), is(true));
+    assertThat("Unexpected record.", found.orElse(null), sameInstance(dbc));
+
+    verify(repository).findById(ID);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldNotFindRecordByIdWhenNotExists() {
+    when(repository.findById(ID)).thenReturn(Optional.empty());
+
+    Optional<Dbc> found = service.findById(ID);
+    assertThat("Record not found.", found.isEmpty(), is(true));
+
+    verify(repository).findById(ID);
+    verifyNoMoreInteractions(repository);
+  }
+
+  @Test
+  void shouldSendRequestWhenNotAlreadyRequested() throws JsonProcessingException {
+    when(requestCacheService.isItemInCache(Dbc.ENTITY_NAME, ID)).thenReturn(false);
+    service.request(ID);
+    verify(dataRequestService).sendRequest("reference", "Dbc", whereMap);
+  }
+
+  @Test
+  void shouldNotSendRequestWhenAlreadyRequested() throws JsonProcessingException {
+    when(requestCacheService.isItemInCache(Dbc.ENTITY_NAME, ID)).thenReturn(true);
+    service.request(ID);
+    verify(dataRequestService, never()).sendRequest("reference", "Dbc", whereMap);
+    verifyNoMoreInteractions(dataRequestService);
+  }
+
+  @Test
+  void shouldSendRequestWhenSyncedBetweenRequests() throws JsonProcessingException {
+    when(requestCacheService.isItemInCache(Dbc.ENTITY_NAME, ID)).thenReturn(false);
+    service.request(ID);
+    verify(requestCacheService).addItemToCache(eq(Dbc.ENTITY_NAME), eq(ID), any());
+
+    dbc.setOperation(DELETE);
+    service.syncRecord(dbc);
+    verify(requestCacheService).deleteItemFromCache(Dbc.ENTITY_NAME, ID);
+
+    service.request(ID);
+    verify(dataRequestService, times(2)).sendRequest("reference", "Dbc", whereMap);
+  }
+
+  @Test
+  void shouldSendRequestWhenRequestedDifferentIds() throws JsonProcessingException {
+    service.request(ID);
+    service.request(ID_2);
+
+    verify(dataRequestService, atMostOnce()).sendRequest("reference", "Dbc", whereMap);
+    verify(dataRequestService, atMostOnce()).sendRequest("reference", "Dbc", whereMap2);
+  }
+
+  @Test
+  void shouldSendRequestWhenFirstRequestFails() throws JsonProcessingException {
+    doThrow(JsonProcessingException.class).when(dataRequestService)
+        .sendRequest(anyString(), anyString(), anyMap());
+
+    service.request(ID);
+    service.request(ID);
+
+    verify(dataRequestService, times(2)).sendRequest("reference", "Dbc", whereMap);
+  }
+
+  @Test
+  void shouldCatchJsonProcessingExceptionIfThrown() throws JsonProcessingException {
+    doThrow(JsonProcessingException.class).when(dataRequestService)
+        .sendRequest(anyString(), anyString(), anyMap());
+    assertDoesNotThrow(() -> service.request(ID));
+  }
+
+  @Test
+  void shouldThrowAnExceptionIfNotJsonProcessingException() throws JsonProcessingException {
+    IllegalStateException illegalStateException = new IllegalStateException("error");
+    doThrow(illegalStateException).when(dataRequestService).sendRequest(anyString(), anyString(),
+        anyMap());
+    assertThrows(IllegalStateException.class, () -> service.request(ID));
+    assertEquals("error", illegalStateException.getMessage());
+  }
+
+  @Test
+  void shouldForwardRecordToReferenceSyncService() {
+    dbc.setOperation(Operation.LOAD);
+    service.syncRecord(dbc);
+
+    verify(referenceSyncService).syncRecord(dbc);
+  }
+}
